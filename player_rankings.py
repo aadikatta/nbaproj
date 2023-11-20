2246d67dd5468d9ad60f24d9ff2a157b90cbe5f2
aadikatta <AADIKATTA@GMAIL.COM>
2023-11-20 10:11:03 -0500

    simplified code

--- player_rankings.py
+++ player_rankings.py
@@ -6,24 +6,16 @@ from nba_api.stats.endpoints import alltimeleadersgrids
 import pandas as pd
 import numpy as np
 
+
+#gets the player ids with the most assists from the endpoint and returnts the players from the player dict
 all_time_points = alltimeleadersgrids.AllTimeLeadersGrids().ast_leaders.get_data_frame()
 player_dict = players.get_players()
 teams_dict = teams.get_teams()
 
-all_time_ast = all_time_points.to_dict()
+all_time_ast = all_time_points.to_dict()['PLAYER_ID'].values()
+ast_final_list = [player['full_name'] for player in player_dict if player['id'] in all_time_ast]
 
-
-#player_names = [player['id'] for player in player_dict if player['id'] in all_time_points['data']]
-#print(all_time_ast[0][0] for player in all_time_ast)
-
-
-ast_player_ids = all_time_ast['PLAYER_ID'].values()
-
-ast_player_list = [player for player in ast_player_ids]
-
-ast_final_list = [player['full_name'] for player in player_dict if player['id'] in ast_player_list]
-
-print(ast_player_list)
+print(all_time_ast)
 print('\n\n')
 print(ast_final_list)
 
